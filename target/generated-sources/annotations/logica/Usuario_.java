bd9d6c1268d4f1cd2b15b2e8a681fc5ebea7c611
= <nicocamilo2010@hotmail.com>
2024-02-22 20:33:39 -0300

    Docker File +add

--- target/generated-sources/annotations/logica/Usuario_.java
+++ target/generated-sources/annotations/logica/Usuario_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2024-02-21T20:52:01")
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2024-02-22T20:15:24")
 @StaticMetamodel(Usuario.class)
 public class Usuario_ { 
 
