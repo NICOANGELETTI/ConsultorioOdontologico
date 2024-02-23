e0807578684c02e965343062fdfba468861887e7
= <nicocamilo2010@hotmail.com>
2024-02-22 23:13:28 -0300

    Dockerfile +add 5

--- target/generated-sources/annotations/logica/Responsable_.java
+++ target/generated-sources/annotations/logica/Responsable_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2024-02-22T22:45:31")
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2024-02-22T22:54:20")
 @StaticMetamodel(Responsable.class)
 public class Responsable_ extends Persona_ {
 
