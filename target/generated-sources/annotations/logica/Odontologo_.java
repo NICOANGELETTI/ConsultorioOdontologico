fb8aa07b5125f63a078156c96267380d8a77cdd4
= <nicocamilo2010@hotmail.com>
2024-02-22 21:40:25 -0300

    Dockerfile +add 3

--- target/generated-sources/annotations/logica/Odontologo_.java
+++ target/generated-sources/annotations/logica/Odontologo_.java
@@ -8,7 +8,7 @@ import logica.Horario;
 import logica.Turno;
 import logica.Usuario;
 
-@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2024-02-22T21:13:40")
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2024-02-22T21:32:46")
 @StaticMetamodel(Odontologo.class)
 public class Odontologo_ extends Persona_ {
 
