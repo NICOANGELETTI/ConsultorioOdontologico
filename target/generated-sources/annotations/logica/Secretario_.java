fa8ba1ebba22478e12a4a9647acddfd37767a727
= <nicocamilo2010@hotmail.com>
2024-02-22 23:20:48 -0300

    Dockerfile +add 6

--- target/generated-sources/annotations/logica/Secretario_.java
+++ target/generated-sources/annotations/logica/Secretario_.java
@@ -5,7 +5,7 @@ import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 import logica.Usuario;
 
-@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2024-02-22T22:54:20")
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2024-02-22T23:20:15")
 @StaticMetamodel(Secretario.class)
 public class Secretario_ extends Persona_ {
 
--- target/generated-sources/annotations/logica/Turno_.java
+++ target/generated-sources/annotations/logica/Turno_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.StaticMetamodel;
 import logica.Odontologo;
 import logica.Paciente;
 
-@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2024-02-22T22:54:20")
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2024-02-22T23:20:15")
 @StaticMetamodel(Turno.class)
 public class Turno_ { 
 
